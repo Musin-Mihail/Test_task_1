880f934437baea4fae908f45bf128cd266448bb7
Мусин Михаил <musin.mihail@gmail.com>
2021-07-14 17:33:07 +0300

    Добивание прикладом

--- Assets/Animation/Player2.cs
+++ Assets/Animation/Player2.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player2 : MonoBehaviour
 {
@@ -10,27 +11,29 @@ public class Player2 : MonoBehaviour
     public int _moveLeft;
     public int _moveRight;
     public int _finishing;
+    public GameObject _finishingText;
     public Transform _chest;
     public Vector3 _mouseVector;
     public Vector3 _target;
     Vector3 _camera = new Vector3(4.07f, 6.69f, -2.9f);
     int _layerMask  = 1 << 8;
-
+    public GameObject _enemy;
+    public GameObject _enemyGun;
+    public Animator _enemyAnimator;
     RaycastHit _hit;
     void LateUpdate()
     {
-        // Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        // if (Physics.Raycast(_ray, out _hit, Mathf.Infinity, _layerMask))
-        // {
-        //     _mouseVector = _hit.point;
-        //     _mouseVector += transform.up*2;
-        //     transform.rotation = Quaternion.LookRotation(Vector3.up, _mouseVector - transform.position);
-        //     transform.transform.Rotate(270,180,0);
-        // }
+        Camera.main.transform.position = transform.position + _camera;
         
-        if(!_animator.GetCurrentAnimatorStateInfo(0).IsName("Finishing"))
+        if(_finishing == 1)
         {
-            Camera.main.transform.position = transform.position + _camera;
+            _chest.rotation = Quaternion.LookRotation(Vector3.up, _target - _chest.position);
+            _chest.transform.Rotate(-30,90,10);
+            transform.rotation = Quaternion.LookRotation(Vector3.up, _target - transform.position);
+            transform.transform.Rotate(270,180,0);
+        }
+        else
+        {
             transform.rotation = Quaternion.identity;
             Ray _ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             if (Physics.Raycast(_ray, out _hit, Mathf.Infinity, _layerMask))
@@ -90,38 +93,62 @@ public class Player2 : MonoBehaviour
             }
             if(Input.GetKeyDown(KeyCode.Mouse0))
             {
-                _animator.Play("Finishing");
-                _target = _mouseVector;                
+                if(_finishingText.activeSelf)
+                {
+                    StartCoroutine(Finishing());
+                }         
             }
-
-            if(Input.GetKeyUp(KeyCode.W))
-            {
-                _moveForward = 0;
-            } 
-            if(Input.GetKeyUp(KeyCode.A))
-            {
-                _moveLeft = 0;
-            } 
-            if(Input.GetKeyUp(KeyCode.D))
-            {
-                _moveRight = 0;
-            } 
-            if(Input.GetKeyUp(KeyCode.S))
-            {
-                _moveBack = 0;
-            } 
-
             if(_moveForward == 0 && _moveBack == 0 && _moveLeft == 0 && _moveRight == 0)
             {
                 _animator.Play("Idle");
             }
         }
-        else
+        if(Input.GetKeyUp(KeyCode.W))
         {
-            _chest.rotation = Quaternion.LookRotation(Vector3.up, _target - _chest.position);
-            _chest.transform.Rotate(-30,90,10);
-            transform.rotation = Quaternion.LookRotation(Vector3.up, _mouseVector - transform.position);
-            transform.transform.Rotate(270,180,0);
+            _moveForward = 0;
+        } 
+        if(Input.GetKeyUp(KeyCode.A))
+        {
+            _moveLeft = 0;
+        } 
+        if(Input.GetKeyUp(KeyCode.D))
+        {
+            _moveRight = 0;
+        } 
+        if(Input.GetKeyUp(KeyCode.S))
+        {
+            _moveBack = 0;
+        } 
+    }
+    void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            _target = other.gameObject.transform.position;
+            _finishingText.SetActive(true);
         }
+    }
+    void OnTriggerExit(Collider other) 
+    {
+        if(other.gameObject.tag == "Enemy")
+        {
+            _finishingText.SetActive(false);
+        }
+    }
+    IEnumerator Finishing()
+    {
+        _finishing = 1;
+        _animator.Play("Run_Rifle");
+        float _distans;
+        _distans = Vector3.Distance(transform.position,_target);
+        while(_distans > 2.5f)
+        {
+            transform.position += transform.forward/30;
+            _distans = Vector3.Distance(transform.position,_target);
+            yield return new WaitForSeconds(0.001f);
+        }
+        _animator.Play("Finishing");
+        yield return new WaitForSeconds(1.5f);
+        _finishing = 0;
     }
 }
