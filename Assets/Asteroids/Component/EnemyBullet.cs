193928ca0eace359fa2c326f6b62997a2e7b1450
Мусин Михаил <musin.mihail@gmail.com>
2025-06-11 19:39:46 +0300

    Перенос

--- Assets/Asteroids/Component/EnemyBullet.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-public class EnemyBullet : MonoBehaviour
-{
-    void Start() 
-    {
-        
-    }
-}
--- Assets/Asteroids/Component/PlayerBullet.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-public class PlayerBullet : MonoBehaviour
-{
-    void Start() 
-    {
-        
-    }
-}
